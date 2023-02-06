a9df42b0b28afcae550abca527612009acf71bbb
Angela Samways <angelasamways@gmail.com>
2023-02-06 20:20:37 -0300

    Req1 com mentoria de Baeta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,24 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+// não remova esse endpoint, e para o avaliador funcionar
+
+const pathTalker = path.resolve(__dirname, './talker.json');
+
+const readTalker = async () => {
+  const read = await fs.readFile(pathTalker, 'utf-8');
+  const talker = JSON.parse(read);
+  console.log(talker);
+  return talker;
+};
+
+app.get('/talker', async (req, res) => {
+  const talkers = await readTalker();
+  if (talkers.length === 0) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
